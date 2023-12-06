a5ae6f7249ff0365a8bd28a6bdbac6233ff85c83
Sasha9873 <asdemkina@gmail.com>
2023-12-06 22:39:50 +0300

    bake in start

--- Assets/sasha-developing/Scripts/nps.cs
+++ Assets/sasha-developing/Scripts/nps.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.AI.Navigation;
 using UnityEngine;
 
 public class nps : MonoBehaviour
@@ -7,12 +8,16 @@ public class nps : MonoBehaviour
     public Transform target;
     private UnityEngine.AI.NavMeshAgent agent;
     public float speed = 10f;
+
+    public NavMeshSurface nav_mesh;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
         agent.speed = speed;
         agent.stoppingDistance = 5;
+
+        nav_mesh.BuildNavMesh();
     }
 
     // Update is called once per frame
