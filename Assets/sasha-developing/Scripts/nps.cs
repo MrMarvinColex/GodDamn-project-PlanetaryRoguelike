22c2ac260ed0926b9973d2b0972d169853516f4e
Sasha9873 <asdemkina@gmail.com>
2023-12-06 21:29:06 +0300

    navmesh and enemy that go to player

---
+++ Assets/sasha-developing/Scripts/nps.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nps : MonoBehaviour
+{
+    public Transform target;
+    private UnityEngine.AI.NavMeshAgent agent;
+    public float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.speed = speed;
+        agent.stoppingDistance = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = target.position;
+    }
+}
