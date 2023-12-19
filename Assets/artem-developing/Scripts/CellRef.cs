07ab7b26bd0606951ae674abf73735c40af57dcd
Артём Тафинцев <tafintsev.aa@phystech.edu>
2023-12-19 23:43:00 +0300

    MapGenerator MVP

---
+++ Assets/artem-developing/Scripts/CellRef.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellRef : MonoBehaviour
+{
+    public int spwfr;
+    public float sizeRect;
+    public Vector3 shift;
+    public GameObject gameObjectj;
+
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
